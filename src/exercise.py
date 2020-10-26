c109c6ad25c9f7925412cda0ba4c870a194c10a6
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-26 14:05:11 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
+import csv
 def main():
     #write your code below this line
+    nameFile = input("Name of the file:")
+    with open(nameFile) as f:
+      reader = csv.reader(f,delimiter=',')
+      for row in reader:
+        if(int(row[1])==1):
+          print("%s, age: %s year"%(row[0],row[1]))
+        else:
+          print("%s, age: %s years"%(row[0],row[1]))
 
 if __name__ == '__main__':
     main()
